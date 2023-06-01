c01c4b814344133dd45d1fae866ddfc97a7aa048
Sira <siraphop_2538@hotmail.com>
2023-06-01 23:35:44 +1000

    Now button can add simple text
    - Create an example text

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("yoyo");
+(()=>{"use strict";class e{noteContainer=document.querySelector(".note-container");createButton=document.querySelector(".create-button");constructor(){this.createNew()}createNote(){const e=document.createElement("div");this.noteContainer.appendChild(e);const t=document.createElement("p");t.textContent="hi",e.appendChild(t)}createNew(){this.createButton.addEventListener("click",(()=>{this.createNote()}))}}new class{constructor(){this.createNote=new e}}})();
